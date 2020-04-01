8595d4ad456180a435d481160e4a724747b0cc7d
ucndaniel <dangrunnet@outlook.com>
2020-04-01 15:45:10 +0200

    Big fucking change mens :bomb:

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,37 @@
 const apiURL = 'http://dangru15.dreamhosters.com/wp-json/wp/v2/';
 const apiKey = 'rBEZbXSsjTQA1fuSUTXTiCpLXUPuTsdq';
 
-getProgramFromWP();
+getProgramFromWP(); // fredag
+getProgramLordagFromWP(); // lørdag
+
+//getBandFromWP(); // ikke færdigt
 
 function getProgramFromWP() {
 
-    const postOptionsId = 729;
+    let postOptionsId = 729;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
                 program = JSON.parse(this.responseText);
                 program_fredag.src = program.acf.program_fredag;
+                
+        }
+    }
+
+    xhttp.open('GET', `${apiURL}posts/${postOptionsId}`, true);
+    xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+    xhttp.send();
+}
+
+
+
+function getProgramLordagFromWP() {
+
+    let postOptionsId = 746;
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+                program = JSON.parse(this.responseText);
                 program_lordag.src = program.acf.program_lordag;
         }
     }
@@ -21,3 +42,24 @@ function getProgramFromWP() {
     xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
     xhttp.send();
 }
+
+
+/*
+function getBandFromWP() {
+    
+    let postOptionsId = 689;
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            band = JSON.parse(this.responseText);
+            band_navn.innerHTML = band.acf.band_navn;
+            genre.innerHTML = band.acf.genre;
+            medlemmer.innerHTML = band.acf.medlemmer;     
+        }
+    }
+    xhttp.open('GET', `${apiURL}posts/${postOptionsId}`, true);
+    xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+    xhttp.send();
+}    problem når det kommer til medlemmer */ 
+
+
